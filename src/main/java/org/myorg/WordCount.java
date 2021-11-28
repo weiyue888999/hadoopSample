b53eb02ef86c4f6a4e83b0965d000f3fca310d59
xiaomo <weiyue8888999@126.com>
2021-11-28 15:01:04 +0800

    init

---
+++ src/main/java/org/myorg/WordCount.java
@@ -0,0 +1,94 @@
+package org.myorg;
+
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.RunningJob;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.TextOutputFormat;
+
+public class WordCount {
+
+	/**
+	 * LongWriable行号
+	 * Text 文本
+	 * Text 文本
+	 * IntWritable 数量
+	 * @author xiaomo
+	 *
+	 */
+	public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+
+		@Override
+		public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+			String line = value.toString();
+			StringTokenizer tokenizer = new StringTokenizer(line);
+			while (tokenizer.hasMoreTokens()) {
+				word.set(tokenizer.nextToken());
+				output.collect(word, one);
+			}
+			
+		}
+	}
+
+	public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+		
+		@Override
+		public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output,Reporter reporter) throws IOException {
+			int sum = 0;
+			while (values.hasNext()) {
+				sum += values.next().get();
+			}
+			output.collect(key, new IntWritable(sum));
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		
+		JobConf conf = new JobConf(WordCount.class);
+		
+		conf.setJobName("wordcount");
+		conf.addResource("classpath:/hadoop/core-site.xml");
+	    conf.addResource("classpath:/hadoop/hdfs-site.xml");
+	    conf.addResource("classpath:/hadoop/mapred-site.xml");
+
+		conf.setOutputKeyClass(Text.class);
+		conf.setOutputValueClass(IntWritable.class);
+
+		conf.setMapperClass(Map.class);
+		conf.setCombinerClass(Reduce.class);
+		conf.setReducerClass(Reduce.class);
+
+		conf.setInputFormat(TextInputFormat.class);
+		conf.setOutputFormat(TextOutputFormat.class);
+
+		{
+			
+			Path path = new Path("hdfs://localhost:9000/user/xiaomo/input");
+			FileInputFormat.setInputPaths(conf, path);
+		}
+
+		{
+			Path path = new Path("hdfs://localhost:9000/user/xiaomo/output");
+			FileOutputFormat.setOutputPath(conf, path);			
+		}
+
+		RunningJob job = JobClient.runJob(conf);
+		job.waitForCompletion();
+	}
+}
